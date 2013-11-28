93620f7cc85e3f7208abde119795825e79322b22
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-11-28 21:50:53 +0100

    update to 4.0.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,6 @@
 class font_explorer_pro {
   package { 'FontExplorerPro':
     provider => 'appdmg_eula_font_explorer_pro',
-    source   => 'http://fex.linotype.com/download/mac/FontExplorerXPro.dmg'
+    source   => 'http://fast.fontexplorerx.com/FontExplorerXPro402.dmg'
   }
 }
--- spec/classes/font_explorer_pro_spec.rb
+++ spec/classes/font_explorer_pro_spec.rb
@@ -4,7 +4,7 @@ describe 'font_explorer_pro' do
   it do
     should contain_package('FontExplorerPro').with({
        :provider => 'appdmg_eula_font_explorer_pro',
-       :source   => 'http://fex.linotype.com/download/mac/FontExplorerXPro.dmg'
+       :source   => 'http://fast.fontexplorerx.com/FontExplorerXPro402.dmg'
     })
   end
 end
